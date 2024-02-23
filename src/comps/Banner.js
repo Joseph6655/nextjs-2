f36f2aeefde497bec04c4a80d4534b18337ffc1e
joseph6655 <josephsassine66@gmail.com>
2024-02-23 11:18:44 +0200

    new chnages

--- src/comps/Banner.js
+++ src/comps/Banner.js
@@ -6,6 +6,12 @@ const Banner = () => {
     <section id="banner">
       <div className="banner-item">
         <img src="/background.jpg"  />
+        <div className="card-content">
+            <h1>We are ready to help you through the journey of creating bespoke software. Our
+team pride themselves in being great problem solvers who will rise to any
+challenge you wish to throw at them and always display a positive ‘can-do’
+attitude.</h1>
+        </div>
       </div>
        <div className="banner-item">
         <img src="/dev.jpg" />
